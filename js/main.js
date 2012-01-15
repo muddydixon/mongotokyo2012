41049f8d2ddb43d929882033a4e4b41e80396482
muddydixon <muddydixon@gmail.com>
2012-01-16 04:31:25 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+	$.deck('.slide');
+	$('#style-themes').change(function() {
+		$('#style-theme-link').attr('href', $(this).val());
+	});
+	$('#transition-themes').change(function() {
+		$('#transition-theme-link').attr('href', $(this).val());
+	});
+	sh_highlightDocument();
+});
